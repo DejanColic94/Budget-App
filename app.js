ee9891beaa5abecee89cdb3956c1589368860e67
Dejan Colic <dejan.colic99@gmail.com>
2021-08-07 22:07:42 +0200

    reading input data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// budet controller module
+// budet controller module -------------------------------------------------------------------------------------------------------------------------------------
 var model = (function() {
 
 
@@ -18,9 +18,28 @@ var model = (function() {
 
 
 
-// UIcontroller module
+// UIcontroller module -------------------------------------------------------------------------------------------------------------------------------------------
 var view = (function() {
 
+    var DOMstrings = {
+        inputType : '.add__type',
+        inputDescription: '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // returns inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 
 
@@ -46,9 +65,31 @@ var view = (function() {
 
 
 
-// App controller, connects model and view
+// App controller, connects model and view -------------------------------------------------------------------------------------------------------------------------------------------
 var controller = (function(model, view) {
 
+    var DOMstrings = view.getDOMstrings();
+    var controllAddItem = function() {
+        // 1. Get input data
+        var input = view.getInput();
+        console.log(input);
+        // 2. Add new item to the DS
+
+        // 3. Display the data in the UI
+
+        // 4. Calculate new total budget
+
+        // 5. Display total budget
+        
+    }
+
+    document.querySelector(DOMstrings.inputBtn).addEventListener('click', controllAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if(event.code === 'Enter') {
+            controllAddItem();
+        }
+    })
 
 
 
