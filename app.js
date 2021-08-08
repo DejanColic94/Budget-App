bfcfbc058966d413c31abdecc4cd32b12527075f
Dejan Colic <dejan.colic99@gmail.com>
2021-08-08 02:23:49 +0200

    display list items

--- app.js
+++ app.js
@@ -77,7 +77,9 @@ var view = (function() {
         inputType : '.add__type',
         inputDescription: '.add__description',
         inputValue : '.add__value',
-        inputBtn : '.add__btn'
+        inputBtn : '.add__btn',
+        incomeContainer : '.income__list',
+        expensesContainer : '.expenses__list'
     }
 
     return {
@@ -88,6 +90,30 @@ var view = (function() {
                 value : document.querySelector(DOMstrings.inputValue).value
             };
         },
+
+        addListItem: function(obj, type) {
+            // placeholder html string
+            var html, newHtml, element;
+
+            if(type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div> <div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+
+            } else if(type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+            }
+            // replace placeholders with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+
+
+            // insert the html into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -137,7 +163,7 @@ var controller = (function(model, view) {
         // 2. Add new item to the DS
         var newItem = model.addItem(input.type, input.description, input.value);
         // 3. Display the data in the UI
-
+        view.addListItem(newItem, input.type);
         // 4. Calculate new total budget
 
         // 5. Display total budget
